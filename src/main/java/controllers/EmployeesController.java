cf3014f63db59332355164905c691c73a6062d97
Gary Muir <garymuir17@gmail.com>
2018-07-17 15:22:52 +0100

    adds employees/:id view

---
+++ src/main/java/controllers/EmployeesController.java
@@ -0,0 +1,46 @@
+package controllers;
+
+import db.DBHelper;
+import db.Seeds;
+import models.Employee;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static spark.Spark.get;
+
+public class EmployeesController {
+
+    public static void main(String[] args) {
+
+        Seeds.seedData();
+
+        ManagersController managersController = new ManagersController();
+
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+
+        get("/employees", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/employees/index.vtl");
+
+            List<Employee> employees = DBHelper.getAll(Employee.class);
+            model.put("employees", employees);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
+
+        get("/employees/:id", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/employees/id.vtl");
+
+            int employeeId = Integer.parseInt(req.params(":id"));
+            Employee employee = DBHelper.find(employeeId, Employee.class);
+
+            model.put("employee", employee);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
+    }
+}
---
+++ src/main/java/controllers/ManagersController.java
@@ -0,0 +1,67 @@
+package controllers;
+
+import db.DBHelper;
+import models.Department;
+import models.Manager;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.HashMap;
+import java.util.List;
+
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+
+public class ManagersController {
+
+    public ManagersController() {
+        this.setUpEndpoints();
+    }
+
+    private void setUpEndpoints() {
+        //TODO: add routes in here
+        VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+
+
+        get("/managers", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/managers/index.vtl");
+
+            List<Manager> managers = DBHelper.getAll(Manager.class);
+            model.put("managers", managers);
+
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
+
+
+        get("/managers/new", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            List<Department> departments = DBHelper.getAll(Department.class);
+            model.put("departments", departments);
+            model.put("template", "templates/managers/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
+
+
+        post("/managers", (req, res) -> {
+            String firstName = req.queryParams("first-name");
+
+            String lastName = req.queryParams("last-name");
+
+            int salary = Integer.parseInt(req.queryParams("salary"));
+
+            int departmentId = Integer.parseInt(req.queryParams("department"));
+            Department department = DBHelper.find(departmentId, Department.class);
+
+            double budget = Double.parseDouble(req.queryParams("budget"));
+
+            Manager newManager = new Manager(firstName, lastName, salary, department, budget);
+            DBHelper.save(newManager);
+
+            res.redirect("/managers");
+            return null;
+        }, velocityTemplateEngine);
+    }
+
+}
