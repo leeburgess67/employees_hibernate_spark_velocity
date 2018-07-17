98dedeba58a7da551d4e8c8f37ec1750dfed3faf
leeburgess67 <leeburgessmail@gmail.com>
2018-07-17 19:35:37 +0100

    delete engineers working

--- src/main/java/controllers/EngineersController.java
+++ src/main/java/controllers/EngineersController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.sun.xml.internal.ws.api.pipe.Engine;
 import db.DBHelper;
 import models.Department;
 import models.Engineer;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
+import static spark.SparkBase.externalStaticFileLocation;
 
 public class EngineersController {
 
@@ -76,10 +78,50 @@ public class EngineersController {
 
 
         //UPDATE
+        get("/engineers/:id/edit", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            List<Department> departments = DBHelper.getAll(Department.class);
+
+            int engineerId = Integer.parseInt(req.params(":id"));
+            Engineer engineer = DBHelper.find(engineerId, Engineer.class);
+
+            model.put("engineer", engineer);
+            model.put("departments", departments);
+            model.put("template", "templates/engineers/edit.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
+
+        post("/engineers/:id", (req, res) -> {
+            int departmentId = Integer.parseInt(req.queryParams("department"));
+            Department department = DBHelper.find(departmentId, Department.class);
+            String firstName = (req.queryParams("first-name"));
+            String lastName = (req.queryParams("last-name"));
+            int salary = Integer.parseInt(req.queryParams("salary"));
+
+            int engineerId = Integer.parseInt(req.params(":id"));
+            Engineer engineer = DBHelper.find(engineerId, Engineer.class);
+            engineer.setFirstName(firstName);
+            engineer.setLastName(lastName);
+            engineer.setSalary(salary);
+            engineer.setDepartment(department);
+            DBHelper.update(engineer);
+
+            res.redirect("/engineers");
+            return null;
+        }, velocityTemplateEngine);
 
 
 
         //DELETE
+        post("/engineers/:id/delete", (req, res) -> {
+            int engineerId = Integer.parseInt(req.params(":id"));
+            Engineer engineer = DBHelper.find(engineerId, Engineer.class);
+            DBHelper.delete(engineer);
+
+            res.redirect("/engineers");
+
+            return null;
+        }, velocityTemplateEngine);
 
 
 
