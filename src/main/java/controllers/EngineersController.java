f45dadcc5ed87162bdf8261c7c9590127d4038ee
leeburgess67 <leeburgessmail@gmail.com>
2018-07-17 18:46:31 +0100

    View individual engineer via link working

--- src/main/java/controllers/EngineersController.java
+++ src/main/java/controllers/EngineersController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import db.DBHelper;
+import models.Department;
 import models.Engineer;
 import models.Manager;
 import org.omg.CORBA.PUBLIC_MEMBER;
@@ -11,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class EngineersController {
 
@@ -22,6 +24,31 @@ public class EngineersController {
         VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
 
         //CREATE
+
+        get("/engineers/new", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            List<Department> departments = DBHelper.getAll(Department.class);
+            model.put("departments", departments);
+            model.put("template", "templates/engineers/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, velocityTemplateEngine);
+
+        post("/engineers", (req, res) -> {
+            String firstName = req.queryParams("first-name");
+
+            String lastName = req.queryParams("last-name");
+
+            int salary = Integer.parseInt(req.queryParams("salary"));
+
+            int departmentId = Integer.parseInt(req.queryParams("department"));
+            Department department = DBHelper.find(departmentId, Department.class);
+
+            Engineer newEngineer = new Engineer(firstName, lastName, salary, department);
+            DBHelper.save(newEngineer);
+
+            res.redirect("/engineers");
+            return null;
+        }, velocityTemplateEngine);
 
 
         //READ
@@ -35,8 +62,21 @@ public class EngineersController {
             return new ModelAndView(model, "templates/layout.vtl");
         }, velocityTemplateEngine);
 
+        get("/engineers/:id", (req, res) -> {
+            HashMap<String, Object> model = new HashMap<>();
+            model.put("template", "templates/engineers/id.vtl");
+
+            int engineerId = Integer.parseInt(req.params(":id"));
+            Engineer engineer= DBHelper.find(engineerId, Engineer.class);
+
+            model.put("engineer", engineer);
+            return new ModelAndView(model, "templates/layout.vtl");
+
+        }, velocityTemplateEngine);
+
 
         //UPDATE
+
 
 
         //DELETE
